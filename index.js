2a55ecda929ae3f12d9017420f92006fc7b74eea
Alister Scott <alister.scott@gmail.com>
2017-09-28 17:32:43 +1000

    Change port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ http.createServer(function (req, res) {
         res.statusCode = 404;
         res.end('no such location');
     });
-}).listen(7777);
+}).listen(443);
 
 handler.on('error', function (err) {
     console.error('Error:', err.message)

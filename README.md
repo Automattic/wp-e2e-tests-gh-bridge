16a79b55ad5f662b2e0bed445b78aff1b8220954
Alister Scott <alister.scott@gmail.com>
2017-10-06 11:24:59 +1000

    Building the bridge (#1)
    * Initial commit
    
    * Fix start script
    
    * Change port
    
    * use port for heroku
    
    * Add port back too
    
    * Got setting label on PR triggering build
    
    * Added updating GH status from CircleCI
    
    * Updated to use canary project
    
    * Move consts to optional env variables
    
    * Update README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # wp-e2e-tests-gh-bridge
-A Github webhooks client that kicks off the e2e tests
+
+A webhooks client that sits between wp-calypso pull requests and e2e canary tests to execute the e2e tests against pull requests and provide status updates.
+
+These provide two webhook paths to do this:
+
+1. https://a8cghcirclebridge.herokuapp.com/ghwebhook: this is the GitHub webhook that takes the pull request event and kicks off a corresponding CircleCI e2e canary test build
+2. https://a8cghcirclebridge.herokuapp.com/circleciwebhook: this is the CircleCI webhook that takes the circleCI webhook when a build is finished and updates the GithHub wp-calypso PR with the appropriate execution result
+
+## Local development using ngrok (on macOS)
+
+1. Install ngrok - `brew install ngrok` on macOS
+2. Set bridge secret `export BRIDGE_SECRET='mysecret'` where `mysecret` is a generated key
+3. Set CircleCI key `export CIRCLECI_SECRET='circlesecret'` where `circlesecret` is a CircleCI API key
+4. Set GitHub Key `export GITHUB_SECRET='githubkey'` where `githubkey` is a GitHub API key
+5. Optionally set `CALYPSO_PROJECT`, `E2E_PROJECT`, `E2E_BRANCH`, `TRIGGER_LABEL` if you wish to override the default values
+6. `npm start` which starts this server on port 7777
+7. In another terminal tab, run `ngrok http 7777` which should provide you a HTTPS url to your localhost server: eg. `https://675bbbef.ngrok.io -> localhost:7777`
+8. Make sure you can access your webhook: eg. `curl https://675bbbef.ngrok.io/circleciwebhook`
+9. Add your ngrok webhook URLs to both Github Project Webhooks (via the UI) and Circle (via `circle.yml`) - when adding the Webhook to GitHub only select the 'Pull Request' event and make sure you choose content type of 'application/json'
+10. As you make changes you will need to re-run `npm start` but ngrok will continue to work
+
+## Deployment to heroku
+
+These webhooks are hosted on heroku. It's easy to deploy using the heroku cli:
+
+`git push heroku` or `git push heroku localbranchname:master -f`(if deploying from non-master)
+
+You can view the logs via heroku or using
+
+`heroku logs`
---
+++ index.js
@@ -0,0 +1,135 @@
+const http = require ( 'http' );
+const request = require ( 'request' );
+const createHandler = require ( 'github-webhook-handler' );
+const url = require( 'url' );
+
+const calypsoProject = process.env.CALYPSO_PROJECT || 'Automattic/wp-e2e-tests-gh-bridge';
+const e2eTestsProject = process.env.E2E_PROJECT || 'Automattic/wp-e2e-tests-for-branches';
+const e2eTestsBranch = process.env.E2E_BRANCH || 'master';
+const triggerLabel = process.env.TRIGGER_LABEL || '[Status] Needs Review';
+
+const triggerBuildURL = `https://circleci.com/api/v1.1/project/github/${ e2eTestsProject }/tree/${ e2eTestsBranch }?circle-token=${ process.env.CIRCLECI_SECRET}`;
+const gitHubStatusURL = `https://api.github.com/repos/${ calypsoProject }/statuses/`;
+
+const gitHubWebHookPath = '/ghwebhook';
+const circleCIWebHookPath = '/circleciwebhook';
+
+const prContext = 'ci/wp-e2e-tests-canary';
+
+const handler = createHandler( { path: gitHubWebHookPath, secret: process.env.BRIDGE_SECRET } );
+
+http.createServer(function (req, res) {
+    const fullUrl = req.url;
+    const path = fullUrl.split( '?' )[0];
+    if ( path === gitHubWebHookPath ) {
+        handler(req, res, function (err) {
+            res.statusCode = 404;
+            res.end('invalid location');
+        });
+    } else if ( path === circleCIWebHookPath ) {
+        console.log( "Called from CircleCI" );
+        let body = [];
+        req.on( 'data', function( chunk ) {
+            body.push( chunk );
+        } ).on( 'end', function() {
+            body = Buffer.concat( body ).toString();
+            try {
+                let payload = JSON.parse( body ).payload;
+                if ( payload && payload.build_parameters && payload.build_parameters.sha && payload.build_parameters.calypsoProject === calypsoProject ) {
+                    let status = 'success';
+                    let desc = 'Your PR passed the e2e canary tests on CircleCI!';
+                    if ( payload.status !== 'success' ) {
+                        status = 'failure';
+                        desc = `Canary test status: ${payload.status}`;
+                    }
+
+                    // POST to GitHub to provide status
+                    let gitHubStatus = {
+                        state: status,
+                        description: desc,
+                        target_url: payload.build_url,
+                        context: prContext
+                    };
+                    request.post( {
+                        headers: { Authorization: 'token ' + process.env.GITHUB_SECRET, 'User-Agent': 'wp-e2e-tests-gh-bridge' },
+                        url: gitHubStatusURL + payload.build_parameters.sha,
+                        body: JSON.stringify( gitHubStatus )
+                    }, function( error ) {
+                        if ( error ) {
+                            console.log( `ERROR: ${error}` );
+                        }
+                        console.log( 'GitHub status updated' );
+                    } );
+                }
+            } catch ( e ) {
+                console.log( 'Non-CircleCI packet received' );
+            }
+            res.statusCode = 200;
+            res.end( 'ok' );
+        } );
+    } else {
+        console.log( 'unknown location', fullUrl );
+        res.statusCode = 404;
+        res.end( 'no such location' );
+    }
+}).listen( process.env.PORT || 7777 );
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message);
+});
+
+handler.on('pull_request', function (event) {
+    if ( event.payload.repository.full_name === calypsoProject && event.payload.action === 'labeled' && event.payload.label.name === triggerLabel ) {
+        const branchName = event.payload.pull_request.head.ref;
+        console.log( 'Executing e2e canary tests for branch: \'' + branchName + '\'' );
+
+        const sha = event.payload.pull_request.head.sha;
+        const pullRequestNum = event.payload.pull_request.number;
+
+        const buildParameters = {
+            build_parameters: {
+                LIVEBRANCHES: 'true',
+                BRANCHNAME: branchName,
+                RUN_ARGS: '-b ' + branchName,
+                sha: sha,
+                pullRequestNum: pullRequestNum,
+                calypsoProject: calypsoProject
+            }
+        };
+
+        // POST to CircleCI to initiate the build
+        request.post( {
+            headers: {'content-type': 'application/json', accept: 'application/json'},
+            url: triggerBuildURL,
+            body: JSON.stringify( buildParameters )
+        } , function( error, response ) {
+            if ( response.statusCode === 201 ) {
+                console.log( 'Tests have been kicked off - updating PR status now' );
+                // Post status to Github
+                const gitHubStatus = {
+                    state: 'pending',
+                    target_url: JSON.parse( response.body ).build_url,
+                    context: prContext,
+                    description: 'The e2e canary tests are running against your PR'
+                };
+                request.post( {
+                    headers: { Authorization: 'token ' + process.env.GITHUB_SECRET, 'User-Agent': 'wp-e2e-tests-gh-bridge' },
+                    url: gitHubStatusURL + sha,
+                    body: JSON.stringify( gitHubStatus )
+                }, function( responseError ) {
+                    if ( responseError ) {
+                        console.log( 'ERROR: ' + responseError  );
+                    }
+                    console.log( 'GitHub status updated' );
+                } );
+            }
+            else
+            {
+                // Something went wrong - TODO: post message to the Pull Request about
+                console.log( 'Something went wrong with executing e2e tests' );
+                console.log( 'ERROR::' + error );
+                console.log( 'RESPONSE::' + JSON.stringify( response ) );
+            }
+        } );
+    }
+});

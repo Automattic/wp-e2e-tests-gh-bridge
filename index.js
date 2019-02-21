bf1d72f057c68cbbcbdc3281cd58ef6f777e1e2f
stojdza <ivanstojadinov@gmail.com>
2019-02-21 16:00:33 +0100

    Trigger full e2e test suite for new PR

--- index.js
+++ index.js
@@ -234,11 +234,10 @@ handler.on( 'pull_request', function( event ) {
 				e2eBranchName = branchName;
 			}
 
-			if ( labelsArray.includes( calypsoFullSuiteTriggerLabel ) ) {
-				description = 'The e2e full WPCOM suite tests are running against your PR';
-				log.info( 'Executing CALYPSO e2e full WPCOM suite tests for branch: \'' + branchName + '\'' );
-				executeCircleCIBuild( 'true', '-S', branchName, e2eBranchName, pullRequestNum, 'ci/wp-e2e-tests-full', '-p -g', description, sha, false, calypsoProject );
-			}
+			description = 'The e2e full WPCOM suite tests are running against your PR';
+			log.info( 'Executing CALYPSO e2e full WPCOM suite tests for branch: \'' + branchName + '\'' );
+			executeCircleCIBuild( 'true', '-S', branchName, e2eBranchName, pullRequestNum, 'ci/wp-e2e-tests-full', '-p -g', description, sha, false, calypsoProject );
+
 			if ( labelsArray.includes( calypsoFullSuiteJetpackTriggerLabel ) ) {
 				description = 'The e2e full Jetpack suite tests are running against your PR';
 				const envVars = { JETPACKHOST: 'PRESSABLEBLEEDINGEDGE' };
